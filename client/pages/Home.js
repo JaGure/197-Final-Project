64cafd54d5f7e503de9d5c3b7002c0dd08ff8626
jagure <jake.gurevitch@gmail.com>
2020-05-12 19:05:44 -0400

    Home page now deals with not logged in user

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 
-const Home = () => {
-    let history = useHistory()
+import Header from '../components/Header'
 
-    const goHome = () => {
-        console.log('asdfasdfasdf')
-        history.push('/signup')
-        console.log('adfasdf')
-    }
+const Home = () => {
+    const [currentUser, setCurrentUser] = useState('')
+    const [userIsLoggedIn, setUserIsLoggedIn] = useState(false)
+    const [groups, setGroups] = useState([])
+    const history = useHistory()
+
+    // called on first render
+    // checks to see if a user is currently logged in, if so, sets the current user
+    // if no user is logged in, redirects to the sign up page
+    useEffect(() => {
+        async function checkIfUserLoggedIn() {
+            // get request to server backend
+            const response = await fetch('/account/', {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+            })
+
+            const resBody = await response.json()
+
+            // if there is a user logged in
+            if (resBody.currentUser != null) {
+                setCurrentUser(resBody.currentUser)
+                setUserIsLoggedIn(true)
+            } else {
+                history.push('/signup') // redirects to the sign up page
+            }
+        }
+        checkIfUserLoggedIn()
+    }, [])
 
     return (
-        <button type="button" onClick={goHome}>?????</button>
+        <>
+            {userIsLoggedIn ? // only shows the home page if the user is logged in so that
+                              // a user getting redirected doesn't accidentally see the home page
+                              // if redirection takes a sec
+                <>
+                    <Header />
+                    <div>
+                        {currentUser}
+                    </div>
+                </> : null}
+        </>
     )
 }
 
