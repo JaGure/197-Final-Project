bd498c6d8c0910dfec301b7ba8f0c357260cca85
jagure <jake.gurevitch@gmail.com>
2020-05-12 11:24:15 -0400

    Added pages and routing between them

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from '../pages/Home'
+import SignUp from '../pages/SignUp'
+import LogIn from '../pages/LogIn'
 
 const App = () => {
     return (
-        <div>Test!</div>
+        <Router>
+            <Switch>
+                <Route path='/signup'>
+                    <SignUp />
+                </Route>
+                <Route path='/login'>
+                    <LogIn />
+                </Route>
+                <Route>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
---
+++ client/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useHistory} from 'react-router-dom'
+
+const Home = () => {
+    let history = useHistory()
+
+    const goHome = () => {
+        console.log('asdfasdfasdf')
+        history.push('/firstPath')
+        console.log('adfasdf')
+    }
+
+    return (
+        <button type="button" onClick={goHome}>?????</button>
+    )
+}
+
+export default Home
