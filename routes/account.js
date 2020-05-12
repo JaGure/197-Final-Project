923b2a0725955c600a6dd3c327a9be80a9620b42
jagure <jake.gurevitch@gmail.com>
2020-05-12 15:34:07 -0400

    Added login POST route

--- routes/account.js
+++ routes/account.js
@@ -5,35 +5,54 @@ const User = require('../models/user').user
 
 const hashPassword = require('../helpers/hashPassword').hashPassword
 
-router.post('/signup', function(req, res, next) {
+router.post('/signup', function (req, res, next) {
     var username = req.body.username
     var password = hashPassword(req.body.password)
 
     // check if the user is in the database, add them if so
     // the response sent will be used by the frontend to tell the user if a username is already taken
-    User.find({ username: username }, function(err, result) {
+    User.find({ username: username }, function (err, result) {
         if (err) {
             next(err)
         } else if (result.length !== 0) {
             res.send({ containedUser: true })
         } else {
             var u = new User({
-                username: username, 
+                username: username,
                 password: password,
                 groups: []
             })
-        
+
             // add user to db
-            u.save(function(err) {
-              if (!err) {
-                res.send({ containedUser: false })
-              } else {
-                next(err)
-              }
+            u.save(function (err) {
+                if (!err) {
+                    res.send({ containedUser: false })
+                } else {
+                    next(err)
+                }
             })
         }
     })
 
-  })
+})
+
+router.post('/login', function (req, res, next) {
+    var username = req.body.username
+    var password = hashPassword(req.body.password)
+
+    // check if there is a user in the database with this password
+    // sends a response corresponding to whether the log in is successful
+    // also establishes a cookie session
+    User.find({ username: username, password: password }, function (err, result) {
+        if (err) {
+            next(err)
+        } else if (result.length === 0) { // login failed
+            res.send({ loginWasSuccessful: false })
+        } else { // login succeeded
+            req.session.username = username
+            res.send({ loginWasSuccessful: true })
+        }
+    })
+})
 
 module.exports = router
