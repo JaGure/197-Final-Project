0fba918b49a88e2173e85caaa10c6a42ee5893ae
jagure <jake.gurevitch@gmail.com>
2020-05-13 01:00:27 -0400

    I hate heroku and mLab and please can this work

--- server.js
+++ server.js
@@ -14,9 +14,17 @@ const userRouter = require('./routes/user')
 const app = express()
 
 // instantiate a mongoose connect call
-mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dnd', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-    .catch(err => console.log(err))
+let dbURI = 'mongodb://heroku_730ww9j3:7u0ca0igjj5i5gtnh682jcpum8@ds015730.mlab.com:15730/heroku_730ww9j3'
+mongoose.connect(dbURI, {
+    useMongoClient: true
+});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// mongoose
+//     .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dnd', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+//     .catch(err => console.log(err))
 
 app.use(express.static(path.join(__dirname, './dist')))
 
