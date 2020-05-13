00601a5c357d465e2e7d6201844c23419fdd8172
jagure <jake.gurevitch@gmail.com>
2020-05-12 21:52:37 -0400

    Frontend can create groups

--- client/components/NewGroupForm.js
+++ client/components/NewGroupForm.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NewGroupForm = props => {
     const { toggleShowButton } = props
+    const [groupName, setGroupName] = useState('')
 
-    const createNewGroup = e => {
+    const createNewGroup = async (e) => {
         e.preventDefault()
+
+        // post request to server backend
+        const response = await fetch('/user/create-group', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ groupName: groupName }),
+        })
         
         toggleShowButton(true)
     }
 
     return (
         <form className='form-inline' onSubmit={e => createNewGroup(e)}>
-            <input type="text" class="form-control mb-2 mr-sm-2" placeholder="Group Name" />
+            <input type="text" class="form-control mb-2 mr-sm-2" value={groupName} onChange={e => setGroupName(e.target.value)} placeholder="Group Name" />
             <button type="submit" class="btn btn-primary mb-2">Submit</button>
         </form>
     )
