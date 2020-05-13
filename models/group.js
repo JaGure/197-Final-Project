bc81086df197cd021aa05267d9c9a09456249083
jagure <jake.gurevitch@gmail.com>
2020-05-12 21:28:24 -0400

    Added create-group route to server

--- models/group.js
+++ models/group.js
@@ -5,12 +5,11 @@ const characterSchema = require('./character').characterSchema
 
 const groupSchema = new mongoose.Schema({
     groupName: { type: String },
-    groupID: { type: String },
     users: { type: [userSchema] }, 
     characters: { 
         type: Map, 
         of:  characterSchema}, // map of users to characters
-    DM: { type: userSchema },    
+    DM: { type: String },    
 })
 
 module.exports = mongoose.model('Group', groupSchema)
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const User = require('../models/user').user
+const Group = require('../models/group')
 
 // responds with the current username
 router.get('/', function (req, res, next) {
@@ -35,6 +36,36 @@ router.get('/groups', function (req, res, next) {
             res.send({ groups: user.groups })
         }
     })
+})
+
+router.post('/create-group', function (req, res, next) {
+    const username = req.session.username
+    const groupName = req.body.groupName
+
+    var g = new Group({
+        groupName: groupName,
+        users = [username],
+        characters: new Map(),
+        DM: username,
+    })
+
+    // add new group to db
+    g.save(function (err) {
+        if(err) {
+            next(err)
+        }
+    })
+
+    // updating user's group list
+    User.findOne({ username: username }, function(err, user) {
+        if (err) {
+            next(err)
+        } else {
+            user.groups.unshift(g._id)
+        }
+    })
+
+    res.status(200).send('Group successfully added')
 })
 
 module.exports = router
