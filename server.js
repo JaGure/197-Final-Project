2cdcd0e049e799292ff7f18ece2b7fda99570984
jagure <jake.gurevitch@gmail.com>
2020-05-13 00:01:18 -0400

    DID I FIX THE HEROKU YET

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const app = express()
 // instantiate a mongoose connect call
 mongoose
     .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dnd', { useNewUrlParser: true, useUnifiedTopology: true }
-    .then(() => console.log('Database Connected'))
     .catch(err => console.log(err))
 )
 
